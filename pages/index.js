f76761631af390537ebbb9cf3026f4aa578bc8b3
Rafi Bagaskara <rafibauu@gagasimaji.com>
2023-04-03 17:07:08 +0700

    Episode 4

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+import db from '../lib/db'
+// import { useEffect } from "react";
+
+export default function Home({ posts }) {
+  console.log({ posts });
+
+  // const createNewPost = async () => {
+  //   await db.post.create({
+  //     data: {
+  //       title: "Ini adalah post selanjutnya",
+  //       content: "Ini adalah content post selanjutnya",
+  //       published: false,
+  //       authorId: "clfi7muxx000008kye7q293q0",
+  //     },
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   createNewPost();
+  // }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +32,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
 
       <footer className={styles.footer}>
@@ -23,12 +41,42 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export async function getServerSideProps(context) {
+  const posts = await db.post.findMany()
+  
+  await db.post.create({
+    data: {
+      title: "Ini adalah post selanjutnya",
+      content: "Ini adalah content post selanjutnya",
+      published: false,
+      authorId: "clfi7muxx000008kye7q293q0",
+    },
+  });
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts,
+    }, // will be passed to the page component as props
+  };
+}
+
+
+
+
+
+
+
+
+
